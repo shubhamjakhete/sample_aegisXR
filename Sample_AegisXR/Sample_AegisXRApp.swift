fdcce7d81c886ed455f70e42c4b1840390384aaa
Shubham Jakhete <shubhamjakhete1997@gmail.com>
2026-02-22 20:54:12 -0800

    Initial Commit

---
+++ Sample_AegisXR/Sample_AegisXRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_AegisXRApp.swift
+//  Sample_AegisXR
+//
+//  Created by Shubham Jakhete on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Sample_AegisXRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
